312f50dc87e763d2245207c2cd53979dfe10370d
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-12 21:59:02 -0400

    Setup Item Model with relationship to Category

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = "items";
+    protected $dates = [ 'created_at', 'updated_at', 'deleted_at'];
+
+    // public function category() {
+    //     return $this->belongsTo("App\Category");
+    // }
+
+    public function category() {
+        return $this->hasOne('App\Company', 'id', 'company_id');
+    }
 }
