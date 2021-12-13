f41f1d032ac81a675568966be2d1e6644d8e5357
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-12 21:57:58 -0400

    Remove specific routes in favor of resource routes

--- routes/web.php
+++ routes/web.php
@@ -20,24 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', 'App\Http\Controllers\HomeController::class@index')->name('home');
-
-// // Category CRUD Routes
-// Route::get('/categories', 'App\Http\Controllers\CategoryController@index');
-// Route::post('/categories', 'App\Http\Controllers\CategoryController@store');
-// Route::get('/categories/{id}', 'App\Http\Controllers\CategoryController@show');
-// Route::get('/categories/{id}/edit', 'App\Http\Controllers\CategoryController@edit');
-// Route::patch('/categories/{id}', 'App\Http\Controllers\CategoryController@update');
-// Route::delete('/categories/{id}', 'App\Http\Controllers\CategoryController@destroy');
-
-// // Item CRUD Routes
-// Route::get('/items', 'App\Http\Controllers\ItemController@index');
-// Route::post('/items', 'App\Http\Controllers\ItemController@store');
-// Route::get('/items/{id}', 'App\Http\Controllers\ItemController@show');
-// Route::get('/items/{id}/edit', 'App\Http\Controllers\ItemController@edit');
-// Route::patch('/items/{id}', 'App\Http\Controllers\ItemController@update');
-// Route::delete('/items/{id}', 'App\Http\Controllers\ItemController@destroy');
 
 // Resource Controller Routes
 Route::resource('public', 'App\Http\Controllers\PublicController');
