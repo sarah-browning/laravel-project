af28c50024a85bcea085e2ad6b64fc4ec26e4393
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-12 23:22:51 -0400

    Add sections for styples, scripts, and partials

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.public')
 
-@section('content')
-    {{-- @php
-        dd($categories);
-    @endphp --}}
-
+@section('content')   
+    top
+    @php
+        // dd($categories);
+    @endphp
     index page
+@endsection
 
+@section('scripts')
+    <script type="text/javascript">
+    
+    </script>
+@endsection
+
+@section('css')
+    <style>
+        p {
+
+        }
+    </style>
 @endsection
