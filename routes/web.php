6a56d346b440a49af43015a959789b5f02c144c1
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-13 14:43:43 -0400

    Changed class name on Home route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'App\Http\Controllers\HomeController::class@index')->name('home');
+Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
 
 // Resource Controller Routes
 Route::resource('public', 'App\Http\Controllers\PublicController');
