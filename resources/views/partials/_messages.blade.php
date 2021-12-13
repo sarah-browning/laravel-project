6564f434bd46ab803cadd1bb8b153ab8c6229252
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-13 14:39:45 -0400

    Fix typos to allow showing all errors

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -10,12 +10,12 @@
     </div>
 @endif
 
-@if ( count($errors) > 0 )
+@if (count($errors) > 0)
     <div class="alert alert-danger" role="alert">
-        <strong>Errors: </strong>
+        <strong>Errors:</strong>
         <ul>
-            #@foreach ($errors->all as $error)
-                <li>{{ $error }}
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
             @endforeach
         </ul>
     </div>
