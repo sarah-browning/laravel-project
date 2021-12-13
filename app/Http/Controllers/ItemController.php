9551dbda97e6545f6d5c4b7662f68c8c503dd33a
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-13 05:20:42 -0400

    Add store function to save items to database

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session;
 
 class ItemController extends Controller
 {
     // Retrieve list of all items, sort by ascending, pass to View
     public function index()
     {
-        $items = \App\Models\Item::all()->sortBy('item');
+        $items = \App\Models\Item::all()->sortBy('title');
         // dd($items);
         return view('items.index')->with('items', $items);
     }
@@ -24,7 +25,31 @@ class ItemController extends Controller
     // Validate input, insert into DB, redirect to index
     public function store(Request $request)
     {
-        dd('store');
+        // dd('store');
+        $rules = [
+            'category_id' => 'required|max:10|numeric',
+            'title' => 'required|unique:items,title|max:100',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'quantity' => 'required|numeric',
+            'sku' => 'required|unique:items,sku|alpha_num',
+            'picture' => 'required|image'
+        ];
+
+        $validator = $this->validate($request, $rules);
+
+        $item = new \App\Models\Item;
+        $item->category_id = $request->category_id;
+        $item->title = $request->title;
+        $item->description = $request->description;
+        $item->price= $request->price;
+        $item->quantity= $request->quantity;
+        $item->sku= $request->sku;
+        $item->picture= $request->picture;
+        $item->save();
+
+        Session::flash('success', 'A new item has been added to the database.');
+        return redirect()->route('items.index');
     }
 
     // Retrieve single item using $id, pass to show view
