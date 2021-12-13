fcafcb0128d5ca330a761011947eddfd148e3c5b
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-13 00:09:36 -0400

    Add Create Category Form to create view

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.public')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create a Category</div>
+
+                    <div class="card-body">
+                        {!! Form::open(['route' => 'categories.store', 'method' => 'post']) !!}
+                            {{ Form::label('category', 'Add Category') }}
+                            {{ Form::text('category', null, ['class'=>'form-control', 'style'=>'', 'id'=>'category' ]) }}
+                            {{ Form::submit('Add Category', ['class'=> 'btn btn-primary btn-lg btn-block', 'style'=>'margin-top:20px'])}}
+                        {!! Form::close() !!}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
