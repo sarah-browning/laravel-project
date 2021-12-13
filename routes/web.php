0ad64603c35f3a129f70240a29f10e30212b1800
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-12 20:56:59 -0400

    Renamed slugs for routes to be pluralized

--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,22 @@ Auth::routes();
 // Route::get('/home', 'App\Http\Controllers\HomeController::class@index')->name('home');
 
 // // Category CRUD Routes
-// Route::get('/category', 'App\Http\Controllers\CategoryController@index');
-// Route::post('/category', 'App\Http\Controllers\CategoryController@store');
-// Route::get('/category/{id}', 'App\Http\Controllers\CategoryController@show');
-// Route::get('/category/{id}/edit', 'App\Http\Controllers\CategoryController@edit');
-// Route::patch('/category/{id}', 'App\Http\Controllers\CategoryController@update');
-// Route::delete('/category/{id}', 'App\Http\Controllers\CategoryController@destroy');
+// Route::get('/categories', 'App\Http\Controllers\CategoryController@index');
+// Route::post('/categories', 'App\Http\Controllers\CategoryController@store');
+// Route::get('/categories/{id}', 'App\Http\Controllers\CategoryController@show');
+// Route::get('/categories/{id}/edit', 'App\Http\Controllers\CategoryController@edit');
+// Route::patch('/categories/{id}', 'App\Http\Controllers\CategoryController@update');
+// Route::delete('/categories/{id}', 'App\Http\Controllers\CategoryController@destroy');
 
 // // Item CRUD Routes
-// Route::get('/item', 'App\Http\Controllers\ItemController@index');
-// Route::post('/item', 'App\Http\Controllers\ItemController@store');
-// Route::get('/item/{id}', 'App\Http\Controllers\ItemController@show');
-// Route::get('/item/{id}/edit', 'App\Http\Controllers\ItemController@edit');
-// Route::patch('/item/{id}', 'App\Http\Controllers\ItemController@update');
-// Route::delete('/item/{id}', 'App\Http\Controllers\ItemController@destroy');
+// Route::get('/items', 'App\Http\Controllers\ItemController@index');
+// Route::post('/items', 'App\Http\Controllers\ItemController@store');
+// Route::get('/items/{id}', 'App\Http\Controllers\ItemController@show');
+// Route::get('/items/{id}/edit', 'App\Http\Controllers\ItemController@edit');
+// Route::patch('/items/{id}', 'App\Http\Controllers\ItemController@update');
+// Route::delete('/items/{id}', 'App\Http\Controllers\ItemController@destroy');
 
 // Resource Controller Routes
 Route::resource('public', 'App\Http\Controllers\PublicController');
-Route::resource('category', 'App\Http\Controllers\CategoryController');
-Route::resource('item', 'App\Http\Controllers\ItemController');
+Route::resource('categories', 'App\Http\Controllers\CategoryController');
+Route::resource('items', 'App\Http\Controllers\ItemController');
