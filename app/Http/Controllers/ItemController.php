8332335c25a95cd17f5352a1795138a06354bfd1
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-12 22:21:42 -0400

    Create skeleton of crud functions for ItemController

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6,79 +6,46 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // Retrieve list of all items, pass to View
     public function index()
     {
-        //
+        $items = \App\Models\Item::all()->sortBy('item');  //sortByDesc
+        dd($items);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // Show View to add new Item
     public function create()
     {
-        //
+        dd('create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // Validate input, insert into DB, redirect to index
     public function store(Request $request)
     {
-        //
+        dd('store');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // Retrieve single item using $id, pass to show view
     public function show($id)
     {
-        //
+        dd('show');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //Retrieve single item using $id, pass to edit view
     public function edit($id)
     {
-        //
+        dd('edit');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // Validate our input, update $id record in DB, redirect to index
     public function update(Request $request, $id)
     {
-        //
+        dd('update');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // Retrieve single item using $id, delete from DB, redirect to index
     public function destroy($id)
     {
-        //
+        dd('destroy');
     }
 }
